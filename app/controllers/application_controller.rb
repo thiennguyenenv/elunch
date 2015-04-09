bd19addb8454002bd9194ee9f20d6b87755bceee
Thien (Theodore) P.X. NGUYEN <theodore@enclave.vn>
2015-04-09 22:57:11 +0700

    no layout for devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
+  layout :layout
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+  def layout
+    devise_controller? ? "subsidy_layout" : "application"
+  end
 end
