c631cb8065bed688d58f94b2bd1b077d843e25bf
Thien (Theodore) P.X. Nguyen <theodore@enclave.vn>
2015-06-10 10:06:15 +0700

    add admin role, and rake db:seed to create admin by default

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150509030506) do
+ActiveRecord::Schema.define(version: 20150610022539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,12 +67,12 @@ ActiveRecord::Schema.define(version: 20150509030506) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 20150509030506) do
     t.integer  "floor_id"
     t.string   "what_your_taste"
     t.integer  "table_id"
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "seed default users - Admin"
+users = [{
+    email: 'admin@elunch.com',
+    password: 'password1',
+    first_name: 'Admin',
+    last_name: 'Admin',
+    admin: true
+  }]
+
+users.each do |user|
+  unless User.find_by_email(user[:email])
+    User.create!(user)
+  end
+end
